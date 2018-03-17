f59645975605e24f89d7afbc1f21acb7a186341c
edennis <eric@5and2studio.com>
2018-03-17 14:12:28 -0500

    add test controller for previewing work

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: "test#test"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  controller :test do
+    get 'test'
+  end
 end
