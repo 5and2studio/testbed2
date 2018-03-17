a94b0026a8eecbb8a0724a139bfb6427ffb3c41b
edennis <eric@5and2studio.com>
2018-03-17 15:55:16 -0500

    added cancancan and configured to use db ability file

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,6 @@
 class TestController < ProtectedController
   def test
+    Rails.logger.info "checking edit user: #{can? :edit, User}"
+    Rails.logger.info "checking read user: #{can? :read, User}"
   end
 end
