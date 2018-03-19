32aa11fae2785783e7f2c2c5fa1e7e8df59b5a88
edennis <eric@5and2studio.com>
2018-03-19 11:22:59 -0500

    add some test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+g = UserGroup.create({ name: "Test Group" })
+p = Permission.create({ name: "Edit User 1", verb: "edit", noun: "User", conditions: "{ \"id\": 1 }" })
+g.permissions << p
+p = Permission.create({ name: "Read User", verb: "read", noun: "User" })
+g.permissions << p
+u = User.create({ first_name: "Test", last_name: "McTesterson", email: "test@test.com", password: "Test123!" })
+u.user_groups << g
