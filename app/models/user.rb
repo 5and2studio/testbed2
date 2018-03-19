b3e9b122487e1ecb22200fd929aa0006cf56ab56
edennis <eric@5and2studio.com>
2018-03-19 12:22:58 -0500

    format dates, make user references "live"

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   
   has_and_belongs_to_many :user_groups
   has_many :permissions, -> { distinct }, through: :user_groups
+
+  def full_name
+    # put a real method here to format the user's name
+    "#{first_name} #{last_name}"
+  end
+
+  def avatar_url
+    #replace this with paperclip or other functions later
+    "/assets/user2-160x160.jpg"
+  end
 end
