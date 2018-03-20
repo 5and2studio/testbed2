6df7666779427f30b7ae82037d95d14ae0f94317
edennis <eric@5and2studio.com>
2018-03-19 22:56:35 -0500

    rework webpack configurations

--- app/javascript/application/index.js
+++ app/javascript/application/index.js
@@ -1,1 +1,4 @@
-import './styles/application.scss';
+import './styles/application.css';
+import './images/user2-160x160.jpg';
+
+console.log('test 123');
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,33 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import 'application';
 require('bootstrap/dist/css/bootstrap.min.css');
 require('font-awesome/css/font-awesome.min.css');
 require('ionicons/dist/css/ionicons.min.css');
 require('jvectormap/jquery-jvectormap.css');
 require('admin-lte/dist/css/AdminLTE.min.css');
 require('admin-lte/dist/css/skins/skin-blue.min.css');
-window.jQuery = window.$ = require('jquery/dist/jquery.min.js');
 
+require('rails-ujs');
+require('turbolinks');
+require('respond/main.js');
+require('html5shiv/src/html5shiv-printshiv.js');
+require('html5shiv/src/html5shiv.js');
+window.jQuery = window.$ = require('jquery/dist/jquery.min.js');
+window.Chart = require('chart.js/Chart.js');
+require('bootstrap/dist/js/bootstrap.min.js');
+require('fastclick/lib/fastclick.js');
+require('admin-lte/dist/js/adminlte.min.js');
+require('jquery-sparkline/jquery.sparkline.js');
+require('jvectormap/jquery-jvectormap.min.js');
+
+import 'application';
+/*
+$(function () {
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square-blue',
+    radioClass: 'iradio_square-blue',
+    increaseArea: '20%' // optional
+  });
+});
+*/
